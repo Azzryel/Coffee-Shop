dca1ed42614be11a00c82008850d8ba435b3bc14
Azzryel <AzryelShroud@gmail.com>
2019-12-29 18:09:35 +0100

    add contact tab template

---
+++ src/contactTab.js
@@ -0,0 +1,13 @@
+function loadContact () {
+    const content = document.querySelector("#content");
+
+    const tabContent = document.createElement("div");
+    tabContent.id = "tab-content";
+    content.appendChild(tabContent);
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "Coffee Shop";
+    tabContent.appendChild(h1);
+} 
+
+export { loadContact }
